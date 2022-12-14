467a950145fd21b3b72620f1ba10920639cdfed1
ThzEdouard <edouard.flaquet@gmail.com>
2022-12-14 18:49:39 +0100

    c00/ex00

--- c00/ex00/megaphone.cpp
+++ c00/ex00/megaphone.cpp
@@ -1,7 +1,21 @@
-
+#include <iostream>
 #include <string>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" ;
+	else
+		for(int i = 1; argv[i]; i++)
+		{
+			for(int y = 0; argv[i][y]; y++)
+			{
+				if (islower(argv[i][y]))
+					argv[i][y] -= 32;
+			};
+		};
+		for(int i = 1; argv[i]; i++)
+			std::cout << argv[i];
+	std::cout << "\n";
+	return (0);
 }
