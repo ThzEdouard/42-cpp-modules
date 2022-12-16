153a4743ca0b2412a68c673d7d44dbbdb2f5657c
ThzEdouard <edouard.flaquet@gmail.com>
2022-12-16 15:09:13 +0100

    commit

--- c00/ex01/contact.hpp
+++ c00/ex01/contact.hpp
@@ -14,12 +14,17 @@ class contact
 		std::string	darkest_secret;
 	public:
 		contact(void);
-		void	add_contact();
+		void	add_contact(void);
 		std::string get_first_name(){	return (this->first_name);}
 		std::string get_last_name(){	return (this->last_name);}
 		std::string get_nickname(){	return (this->nickname);}
 		std::string get_phone_number(){	return (this->phone_number);}
 		std::string get_darkest_secret(){	return (this->darkest_secret);}
 };
+
+void	contact::add_contact(void)
+{
+
+}
 
 #endif
---
+++ c00/ex01/phonebook.hpp
@@ -0,0 +1,14 @@
+#ifndef PHONEBOOK_H
+# define PHONEBOOK_H
+
+# include "contact.hpp"
+
+class phonebook
+{
+	private:
+		contact contact[8];
+	public:
+		phonebook();
+};
+
+#endif
