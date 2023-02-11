98f768e0976a74159807b3f7068b526bfbf390e2
ThzEdouard <edouard.flaquet@gmail.com>
2023-02-11 11:14:07 +0100

    cpp

---
+++ c01/ex00/Zombie.cpp
@@ -0,0 +1,25 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie()
+{
+}
+
+Zombie	*Zombie::newZombie(std::string n)
+{
+
+}
+
+void	Zombie::randomChump(std::string n)
+{
+
+}
+
+void	Zombie::announce(void)
+{
+	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie::~Zombie()
+{
+
+}
