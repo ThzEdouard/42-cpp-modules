73f16fc4da2a43544afec1ff06a4fedd029d5fb2
ThzEdouard <edouard.flaquet@gmail.com>
2023-06-15 08:03:07 +0200

    c06

--- c06/ex01/main.cpp
+++ c06/ex01/main.cpp
@@ -17,24 +17,14 @@ Data* deserialize(uintptr_t raw)
 
 int main()
 {
-    // Création d'une instance de t_Data
     Data data;
     data.str = "Hello, world!";
 
-    // Sérialisation de l'instance
     uintptr_t serialized = serialize(&data);
-
-    // Affichage de l'adresse sérialisée
     std::cout << "Adresse sérialisée : " << serialized << std::endl;
 
-    // Désérialisation de l'adresse sérialisée
     Data* deserialized = deserialize(serialized);
-
-    // Vérification du contenu désérialisé
     std::cout << "Contenu désérialisé : " << deserialized->str << std::endl;
-
-    // Libération de la mémoire (si nécessaire)
-    // delete deserialized;
 
     return 0;
 }
